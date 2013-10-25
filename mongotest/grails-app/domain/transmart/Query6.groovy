c477f41ea79a09db1d73313c2af4b849ed2478e9
Chao Wu <wuchaozju@gmail.com>
2013-10-25 12:29:48 +0100

    query6

---
+++ mongotest/grails-app/domain/transmart/Query6.groovy
@@ -0,0 +1,20 @@
+package transmart
+
+class Query6 {
+	int patient_id
+	double log_intensity
+	String raw_intensity
+	int assay_id
+	String probe_id
+	int probeset_id
+	String sourcesystem_cd
+	String gpl_id
+	String trial_name
+	double zscore
+	String gene_symbol
+	int gene_id
+	int sample_type_cd
+
+    static constraints = {
+    }
+}
